42688100f82460358cb35b9c83e19fdac3feb1f3
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2026-06-02 15:47:13 -0300

    fix: Correção na URL do api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuração do Axios para a API do RAWG
 const api = axios.create({
-  baseURL: "https://api/rawg.io/api",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: import.meta.env.VITE_RAWG_API_KEY,
   },
